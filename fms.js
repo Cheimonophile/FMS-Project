e85ebe7a2eea04711d3e32dd89c8fb3f96e857ad
Wuthna <79997453+Wuthna@users.noreply.github.com>
2021-03-10 10:41:22 -0700

    Update fms.js

--- fms.js
+++ fms.js
@@ -4,7 +4,7 @@ const HEIGHT = 1136;
 
 // Point Class for storing points
 class Point {
-  constructor(x,y) {
+  constructor(x, y) {
     this.x = x;
     this.y = y;
   }
@@ -15,12 +15,12 @@ class Point {
 // Class that keeps track of the state of the app
 // Ben
 class State {
-  
+
   constructor() {
     this.exercise = new Exercise1();
-    this.button1 = new Point(WIDTH/4, HEIGHT-100);
-    this.button2 = new Point(2*WIDTH/4, HEIGHT-100);
-    this.button3 = new Point(3*WIDTH/4, HEIGHT-100);
+    this.button1 = new Point(WIDTH / 4, HEIGHT - 100);
+    this.button2 = new Point(2 * WIDTH / 4, HEIGHT - 100);
+    this.button3 = new Point(3 * WIDTH / 4, HEIGHT - 100);
   }
 }
 
@@ -36,46 +36,45 @@ class Exercise2 {}
 // the state of exercise 3
 // Adrian
 class Exercise3 {
-// this function would randomly generate circles which would mimic doorknob
-var circles = []; 
+  // this function would randomly generate circles which would mimic doorknob
+  constructor() {
+    var circles = []; 
+    this.exercise03 = new Exercise3(); 
+    this.button1 = new Point(1*WIDTH/6, HEIGHT-100);
+    this.button2 = new Point(3*WIDTH/6, HEIGHT-100);
+    this.button3 = new Point(5*WIDTH/6, HEIGHT-100);
+  }
 
-function setup() {
-// createCanvas(640, 1136);
-  
-  // for (let i = 0; i < 5; i++) {
-  //   fill(random(255), random(255), random(255));
-  //   let size = 100;
-  //   ellipse(random(400)+50, random(400)+50, size, size)
-  // }
-  
-  for (var i = 0; i < 10; i++) {
-    var circle = {
-      x: random(width),
-      y: random(height),
-      r: random(12, 36)
-    }; 
-  
-    var overlapping = false; 
-  
-    for (var j = 0; j < circles.length; j++) {
-    var other = circles[j];
-    var d = dist(circle.x, circle.y, other.x, other.y);
-    if (d < circle.r + other.r) {
-      overlapping = true; 
+   setup() {
+    var circles = [];
+    for (var i = 0; i < 10; i++) {
+      var circle = {
+        x: random(width),
+        y: random(height),
+        r: random(12, 36)
+      };
+
+      var overlapping = false;
+
+      for (var j = 0; j < circles.length; j++) {
+        var other = circles[j];
+        var d = dist(circle.x, circle.y, other.x, other.y);
+        if (d < circle.r + other.r) {
+          overlapping = true;
+        }
+      }
+
+      for (var i = 0; i < circles.length; i++) {
+        fill(255, 0, 150, 100);
+        noStroke();
+        ellipse(circles[i].x, circles[i].y, circles[i].r * 2, circles[i].r * 2);
+      }
+
+      if (!overlapping) {
+        circles.push(circle);
       }
     }
-  
-  for (var i = 0; i < circles.length; i++) {
-    fill(255, 0, 150, 100);
-    noStroke();
-    ellipse(circles[i].x, circles[i].y, circles[i].r*2, circles[i].r*2);
   }
-    
-   if (!overlapping) {
-    circles.push(circle);
-   }
-  }
-}
 }
 
 
@@ -94,4 +93,3 @@ function setup() {
 function draw() {
   background(220);
 }
-
